7ed1f21a719a66a655e3db82896910ba00126c1d
nickmagruder <nick.magruder@outlook.com>
2022-05-03 00:41:39 -0700

    user login socket built

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
-import { Server } from "socket.io";
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
-const io = new Server(3000);
+const app = express();
+const expressServer = createServer(app);
+const PORT = 3001;
+const io = new Server(expressServer, {
+  cors: {
+    origin: '*',
+  },
+});
 
-io.on("connection", (socket) => {
-  socket.emit("hello from server", 1, "2", { 3: Buffer.from([4]) });
+let sockets = {};
+let users = {};
 
-  socket.on("hello from client", (...args) => {
-    // ...
+io.on('connection', (client) => {
+  console.log(client.id + ' has connected');
+  client.emit('connected', { id: client.id });
+
+  client.on('checkUserName', (data) => {
+    console.log(data);
+    let flag = false;
+    for (let id in sockets) {
+      if (sockets[id].userName === data.name) {
+        flag = true;
+        break;
+      }
+    }
+    if (!flag) {
+      sockets[client.id] = {
+        userName: data.name,
+      };
+    }
+    client.emit('userNameResponse', !flag);
   });
 });
+
+expressServer.listen(PORT);
+console.log('Server listening on Port: ' + PORT);
