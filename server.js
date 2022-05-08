9319a2073aba2110d69362020e6b33299415d490
nickmagruder <nick.magruder@outlook.com>
2022-05-08 02:24:29 -0700

    cors

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const expressServer = createServer(app);
 const PORT = process.env.PORT;
 const io = new Server(expressServer, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://tic-tac-toe-whoa.herokuapp.com/',
+    ],
   },
 });
 
